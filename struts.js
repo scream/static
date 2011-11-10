384f84edf85c58fed7042f3b8d57e5f90b86ff53
seven.guo <rubikguo@gmail.com>
2011-11-10 18:08:13 +0800

    scream

--- struts.js
+++ struts.js
@@ -1,8 +1,1 @@
-pl_history_title.png
-pl_history_view.png
-paging_arrow_bg.png
-etBookbyteacher.js
-etTeacherScheduleSingleSubout.js
-bookbyteacherpopup.css
-bookbyteacher-popup.cshtml
-teacher-subout-popup.cshtml
+struts.js
